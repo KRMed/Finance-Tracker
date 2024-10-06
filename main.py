7ff8cd8ba8d09b7a183521a9b85566033d36bcde
krist <kmedeiros@ufl.edu>
2024-10-06 14:30:18 -0400

    added commit

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ class CSV:
             df = pd.DataFrame(columns=cls.Columns)
             df.to_csv(cls.csv_file, index=False)
 
-#comment o
     @classmethod
     def add_entry(cls, date, amount, category, description):
         new_entry = {
